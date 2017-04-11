c3bcc04e39045c56d99cefe187490321ee26c516
Elliot <elliot.ayrey@gmail.com>
2017-04-11 15:41:35 +1200

    11apr17: fixed indentation.

--- src/nz/ac/ara/ayreye/theseusandtheminotaur/actual/Game.java
+++ src/nz/ac/ara/ayreye/theseusandtheminotaur/actual/Game.java
@@ -9,45 +9,45 @@ public class Game implements Playable, Loadable, Saveable {
 	protected List<List<Cell>> level = new ArrayList<List<Cell>>();
 	protected int width = 0;
 	protected int depth = 0;
-	
+
 	/*
 	 * Private Methods
 	 */
-	
+
 	private Cell getCell(MyPoint where) {
 		List<Cell> row = level.get(where.down());
 		Cell cell = row.get(where.across());
 		return cell;
 	}
-	
+
 	private void setCell(MyPoint where, Wall wall, String flag) {
 		int rowIndex = where.down();
 		int cellIndex = where.across();
 		List<Cell> rowCopy = level.get(rowIndex);
 		Cell cellCopy = rowCopy.get(cellIndex);
-		
+
 		if (flag == "top") {
 			cellCopy.top = wall;
 		} else if (flag == "left") {
 			cellCopy.left = wall;
 		}
-		
+
 		rowCopy.set(cellIndex, cellCopy);
 		level.set(rowIndex, rowCopy);
 	}
-	
+
 	private void setCell(MyPoint where, Actor actor) {
 		int rowIndex = where.down();
 		int cellIndex = where.across();
 		List<Cell> rowCopy = level.get(rowIndex);
 		Cell cellCopy = rowCopy.get(cellIndex);
-		
+
 		cellCopy.actor = actor;
-		
+
 		rowCopy.set(cellIndex, cellCopy);
 		level.set(rowIndex, rowCopy);
 	}
-	
+
 	private void build() {
 		for (int i = 0; i < this.depth; i++) {
 			List<Cell> row = new ArrayList<Cell>();
@@ -57,17 +57,15 @@ public class Game implements Playable, Loadable, Saveable {
 			level.add(row);
 		}
 	}
-	
+
 	/*
-	 * Currently only finds first occurrence..
-	 * TODO: ensure only ONE occurrence?
-	 * TODO: Make input parameter generic 
-	 * if ACTOR enum is converted to classes.
+	 * Currently only finds first occurrence.. TODO: ensure only ONE occurrence?
+	 * TODO: Make input parameter generic if ACTOR enum is converted to classes.
 	 * TODO: Rename (to 'find()'?)
 	 */
 	private MyPoint findActor(Actor actor) {
 		MyPoint result = null;
-		
+
 		for (int i = 0; i < this.depth; i++) {
 			for (int j = 0; j < this.width; j++) {
 				MyPoint here = new DefaultPoint(j, i);
@@ -76,42 +74,39 @@ public class Game implements Playable, Loadable, Saveable {
 				}
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 	// [8/8] Tests passing
 	public boolean isBlocked(Direction direction, MyPoint current, MyPoint destination) {
 		boolean result = false;
-		
+
 		if (direction == Direction.LEFT) {
 			if (this.getCell(current).left == Wall.SOMETHING) {
 				result = true;
 			}
-		}
-		else if (direction == Direction.RIGHT) {
+		} else if (direction == Direction.RIGHT) {
 			if (this.getCell(destination).left == Wall.SOMETHING) {
 				result = true;
 			}
-		}
-		else if (direction == Direction.UP) {
+		} else if (direction == Direction.UP) {
 			if (this.getCell(current).top == Wall.SOMETHING) {
 				result = true;
 			}
-		}
-		else if (direction == Direction.DOWN) {
+		} else if (direction == Direction.DOWN) {
 			if (this.getCell(destination).top == Wall.SOMETHING) {
 				result = true;
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 	// OLD (unneeded?)
 	public Direction findDirection(MyPoint theseus, MyPoint minotaur) {
 		Direction result = null;
-		
+
 		if (theseus.across() > minotaur.across()) {
 			result = Direction.RIGHT;
 		} else if (theseus.across() < minotaur.across()) {
@@ -121,13 +116,13 @@ public class Game implements Playable, Loadable, Saveable {
 		} else if (theseus.down() < minotaur.down()) {
 			result = Direction.UP;
 		}
-		
+
 		return result;
 	}
-	
+
 	public Direction findDirection(MyPoint theseus, MyPoint minotaur, String flag) {
 		Direction result = null;
-		
+
 		if (flag == "horizontal") {
 			if (theseus.across() > minotaur.across()) {
 				result = Direction.RIGHT;
@@ -141,14 +136,14 @@ public class Game implements Playable, Loadable, Saveable {
 				result = Direction.UP;
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 	/*
 	 * <<Interface>> Saveable
 	 */
-	
+
 	@Override
 	public int getWidthAcross() {
 		// assertNotNull(this.width);
@@ -189,28 +184,26 @@ public class Game implements Playable, Loadable, Saveable {
 	/*
 	 * <<Interface>> Loadable
 	 */
-	
+
 	@Override
 	public int setWidthAcross(int widthAcross) {
 		this.width = widthAcross;
-		
-		if (this.depth > 0
-				&& this.width > 0) {
+
+		if (this.depth > 0 && this.width > 0) {
 			this.build();
 		} // TODO: else throw exception?
-		
+
 		return this.width; // ?
 	}
 
 	@Override
 	public int setDepthDown(int depthDown) {
 		this.depth = depthDown;
-		
-		if (this.width > 0
-				&& this.depth > 0) {
+
+		if (this.width > 0 && this.depth > 0) {
 			this.build();
 		} // TODO: else throw exception
-		
+
 		return this.depth; // ?
 	}
 
@@ -242,7 +235,7 @@ public class Game implements Playable, Loadable, Saveable {
 	/*
 	 * <<Interface>> Game
 	 */
-	
+
 	// TODO: Move to CONTROLLER?
 	private boolean winState() {
 		boolean result = false;
@@ -251,9 +244,9 @@ public class Game implements Playable, Loadable, Saveable {
 		}
 		return result;
 	}
-	
-	/* NEW_FEATURE 
-	 * [20/20] Tests passing
+
+	/*
+	 * NEW_FEATURE [20/20] Tests passing
 	 */
 	@Override
 	public void moveTheseus(Direction direction) {
@@ -275,10 +268,10 @@ public class Game implements Playable, Loadable, Saveable {
 		MyPoint theseusAt = this.wheresTheseus();
 		MyPoint minotaurAt = this.wheresMinotaur();
 		MyPoint destination;
-		
+
 		Direction horizDir = this.findDirection(theseusAt, minotaurAt, "horizontal");
 		Direction vertDir = this.findDirection(theseusAt, minotaurAt, "vertical");
-		
+
 		if (horizDir != null
 				&& !this.isBlocked(
 						horizDir,
@@ -287,10 +280,10 @@ public class Game implements Playable, Loadable, Saveable {
 								minotaurAt.across() + horizDir.xAdjust,
 								minotaurAt.down() + horizDir.yAdjust))) {
 			this.setCell(minotaurAt, Actor.NONE);
-			this.addMinotaur(destination);			
+			this.addMinotaur(destination);
 		} else if (vertDir != null
 				&& !this.isBlocked(
-						vertDir,
+						vertDir, 
 						minotaurAt,
 						destination = new DefaultPoint(
 								minotaurAt.across() + vertDir.xAdjust,
